47d611b8f1d7e44af6bcf128e620667173715202
Ramiro Magno <ramiro.magno@gmail.com>
2022-11-10 00:01:58 +0000

    Fix #1

--- R/parse_declaration.R
+++ R/parse_declaration.R
@@ -2,10 +2,10 @@
 parse_declaration <- function(x) {
 
   format <- stringr::str_match(x, pattern = '^(variableStep|fixedStep)')[, 2]
-  chrom <- stringr::str_match(x, pattern = 'chrom=(\\w+)')[, 2]
-  start <- stringr::str_match(x, pattern = 'start=(\\w+)')[, 2]
-  step <- stringr::str_match(x, pattern = 'step=(\\w+)')[, 2]
-  span <- stringr::str_match(x, pattern = 'span=(\\w+)')[, 2]
+  chrom <- stringr::str_match(x, pattern = 'chrom=(\\S+)')[, 2]
+  start <- stringr::str_match(x, pattern = 'start=(\\S+)')[, 2]
+  step <- stringr::str_match(x, pattern = 'step=(\\S+)')[, 2]
+  span <- stringr::str_match(x, pattern = 'span=(\\S+)')[, 2]
 
   list(format = format, chrom = chrom, start = as.integer(start), step = as.integer(step), span = as.integer(span))
 
